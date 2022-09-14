d60913b7e7bc926d99a1464c5d951d698bd822df
sdu2030 <86030593+sdu2030@users.noreply.github.com>
2022-09-14 15:21:14 -0400

    Add files via upload
    added multi threading

--- pyAutoClicker.py
+++ pyAutoClicker.py
@@ -2,6 +2,7 @@
 try: 
    import pyautogui
    import keyboard
+   import threading
 except:
    import pip
    pip.main(['install', 'pyautogui'])
@@ -11,11 +12,45 @@ except:
 
 # Actual Program
 
-while True:
-   if keyboard.is_pressed('c'):
-      while True:
-         pyautogui.click()
+def autoClick():
+   while True:
+      pyautogui.click()
+      
+      # Toggles the autoclicker once 'c' is pressed on the keyboard
+      if keyboard.is_pressed('c'):
+         break
+
+   
+if __name__ =="__main__":
+
+   # Prepares threads for multi-threading
+   t1 = threading.Thread(target=autoClick)
+   t2 = threading.Thread(target=autoClick)
+   t3 = threading.Thread(target=autoClick)
+   t4 = threading.Thread(target=autoClick)
+   t5 = threading.Thread(target=autoClick)
+   
+   while True:
+      if keyboard.is_pressed('c'):
+
+         # Toggles the autoclicker once 'c' is pressed on the keyboard
+         t1.start()
+         t2.start()
+         t3.start()
+         t4.start()
+         t5.start()
          
-         if keyboard.is_pressed('c'):
-            pyautogui.alert('The autoclicker has been stopped.')
-            break       
+         # Joins threads so the alert message only pops up after the threads are exited
+         t1.join()
+         t2.join()
+         t3.join()
+         t4.join()
+         t5.join()
+         pyautogui.alert('The autoclicker has been stopped.')
+         
+         # Redefines threads so they can be started a second time
+         t1 = threading.Thread(target=autoClick)
+         t2 = threading.Thread(target=autoClick)
+         t3 = threading.Thread(target=autoClick)
+         t4 = threading.Thread(target=autoClick)
+         t5 = threading.Thread(target=autoClick)
